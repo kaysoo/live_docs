11f55f51a2dfb8c8f5ab570b29f351b8f3d85ed2
kaysoo22 <kofisarfo466@gmail.com>
2024-12-09 21:20:48 +0000

    worked on clerk authentication

---
+++ app/Provider.tsx
@@ -0,0 +1,18 @@
+"use client";
+import Loader from "@/components/loader";
+import {
+  ClientSideSuspense,
+  LiveblocksProvider,
+} from "@liveblocks/react/suspense";
+
+import React from "react";
+
+const Provider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <LiveblocksProvider authEndpoint={"/api/liveblocks-auth"}>
+      <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
+    </LiveblocksProvider>
+  );
+};
+
+export default Provider;
